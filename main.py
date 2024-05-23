bcae535b38f2b2ef5aa3cdc0e9b575fe8352573d
Truong812001 <108712462+Truong812001@users.noreply.github.com>
2024-05-23 13:30:26 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-
+import cmath
+import openpyxl
 
 class NRS:
 
@@ -12,18 +13,21 @@ class NRS:
         self.general = pd.read_excel(file, sheet_name='general').values
         self.xy = pd.read_excel(file, sheet_name='coordinates').iloc[:, 1:].values
         self.loads = pd.read_excel(file, sheet_name='loads').values
+        self.shunt = pd.read_excel(file, sheet_name='shunt').values
+        self.cap = pd.read_excel(file, sheet_name='capacitor_bank').values
         self.feeder=self.Ybus()
     def Ybus(self):
 
         '''get data'''
-        lines, codes, general, xy = self.lines, self.codes, self.general, self.xy
+        lines, codes, general, xy ,shunt = self.lines, self.codes, self.general, self.xy, self.shunt
 
         p_base = general[0, 1] / 3  # Công suất danh định theo pha
+##        print(p_base)
         v_base = general[0, 0] / np.sqrt(3)  # Điện áp dây đến trung bình
-        print(v_base)
+##        print(v_base)
         z_base = v_base**2 / p_base
-        print(z_base)
-        print(p_base)
+##        print(z_base)
+##        print(p_base)
         num_n = int(np.max(np.max(lines[:, :2])))  # Số nút trong mạng
         num_l = len(lines)  # Số đoạn dây
         ybus = np.zeros((3 * num_n, 3 * num_n), dtype=complex)  # Ma trận dẫn suất YBus
@@ -41,7 +45,9 @@ class NRS:
             zm = (r0 - r1) / 3 + (x0 - x1) / 3 * 1j ##
 
             z_line[:, :, k] = np.array([[zs, zm, zm], [zm, zs, zm], [zm, zm, zs]]) / z_base ## matrix
+
             yL = np.linalg.inv(z_line[:, :, k])
+
 
             nt1 = [int(n1), int(n1 + num_n), int(n1 + 2 * num_n)]       ## Node i
             nt2 = [int(n2), int(n2 + num_n), int(n2 + 2 * num_n)]       ## Node j
@@ -53,6 +59,19 @@ class NRS:
                     ybus[nt2[i]-1,nt1[j]-1] -= yL[i][j]     ## Node ji
                     ybus[nt2[i]-1,nt2[j]-1] += yL[i][j]     ## Node jj
 
+
+        ## Shunt
+        # for i in range(len(shunt)):
+        #     n1 = int(shunt[i,0])
+        #     ph = int(shunt[i,1])
+        #     q = shunt[i,2]
+
+        #     if ph == 1 :
+        #         ybus[n1-1,n1-1] -= 1j* q * z_base / (v_base**2)     ##  z_base = v_base**2 / p_base
+        #     elif ph==2:
+        #         ybus[n1+num_n-1,n1+num_n-1] -= 1j* q * z_base /(v_base**2)
+        #     elif ph==3:
+        #         ybus[n1+2*num_n-1,n1+2*num_n-1] -= 1j* q * z_base /(v_base**2)
 
 
 
@@ -102,10 +121,13 @@ class NRS:
             elif ph == 3:
                 s_load[n1 + 2 * num_n - 1] = p + 1j * q
         return s_load
+
+
     def main(self):
         num_n=self.feeder['num_n']
         ybus=self.feeder['ybus']
 
+        np.savetxt('ybus.txt', ybus, fmt='%1.7e')
         G = np.real(ybus)
         B = np.imag(ybus)
 
@@ -145,7 +167,7 @@ class NRS:
 
 
         err = 100
-        conv = np.zeros(10)
+        conv = np.zeros(100)
         iter = 1
         num_t = 3 * num_n       # tổng nút
         num_r = len(n_other)    # khac slack
@@ -156,15 +178,14 @@ class NRS:
         J = np.zeros((num_t, num_t))
         L = np.zeros((num_t, num_t))
         ybus=pd.DataFrame(ybus)
-        np.savetxt('ybus.txt', ybus, fmt='%1.7e')
+##        np.savetxt('ybus.txt', ybus, fmt='%1.7e')
         while err > 1E-9:
 
             vn = v * np.exp(an * 1j)
-            abc=np.conj(np.dot(ybus,vn))
-            np.savetxt('abc.txt', abc, fmt='%1.7e')
+
+
             sn = vn * np.conj(np.dot(ybus ,vn))
-##            print('vn',vn)
-##            print('sn',sn)
+
             p = np.real(sn)
             q = np.imag(sn)
 
@@ -179,8 +200,9 @@ class NRS:
                         akm = an[k] - an[m]
                         N[k, m] = v[k] * (G[k, m] * np.cos(akm) + B[k, m] * np.sin(akm))
                         L[k, m] = v[k] * (G[k, m] * np.sin(akm) - B[k, m] * np.cos(akm))
-                        H[k, m] = L[k, m] * v[m]
+                        H[k, m] = L[k, m] * v[m]    ## check
                         J[k, m] = -N[k, m] * v[m]
+
             dp=np.zeros((num_r,1))
             dq=np.zeros((num_r,1))
 
@@ -189,12 +211,10 @@ class NRS:
                 n_slack1[i] = n_slack[i]-1
 
 ##            n_slack =[0,3,6]
-            H1=H
-            N1=N
-            J1=J
-            L1=L
-
-
+            H1=H.copy()
+            N1=N.copy()
+            J1=J.copy()
+            L1=L.copy()
 
 
             H1 = np.delete(H1, n_slack1, axis=0)
@@ -215,10 +235,6 @@ class NRS:
                 dem+=1
             np.savetxt('pref.txt', pref, fmt='%1.7e')
             np.savetxt('qref.txt', qref, fmt='%1.7e')
-##            print(pref)
-##            print(qref)
-##            print(p)
-##            print(q)
             dpq=np.concatenate((dp,dq))
             np.savetxt('dpq.txt', dpq, fmt='%1.7e')
 
@@ -228,13 +244,6 @@ class NRS:
             Jac = pd.DataFrame(Jac)
             np.savetxt('Jac.txt', Jac, fmt='%1.7e')
 
-
-
-
-
-
-
-
             dx = np.linalg.solve(Jac, dpq)
             np.savetxt('dx.txt', dx, fmt='%1.7e')
             dem=0
@@ -242,16 +251,12 @@ class NRS:
                 an[i-1] += dx[dem]
                 v[i-1] += dx[dem+num_r]
                 dem+=1
-            np.savetxt('an.txt', an, fmt='%1.7e')
-            np.savetxt('v.txt', v, fmt='%1.7e')
-
-
 
             err = np.linalg.norm(dx)
             conv[iter - 1] = err
             iter += 1
 
-            if iter > 10:
+            if iter > 100:
                 print('Phương pháp Newton. Sau 10 lần lặp, sai số là:', err)
                 break
         vn = v * np.exp(an * 1j)
@@ -262,7 +267,30 @@ class NRS:
         res['p_loss'] = np.real(np.sum(res['s_node']))
         res['error'] = conv
         res['iter'] = iter
-        print(res)
-nrs=NRS('FEEDER901.xlsx')
-print(nrs.main())
+        return res,v
+    def report(self):
+        res,v=self.main()
+        num_n = self.feeder['num_n']
+        a=[]
+        b=[]
+        c=[]
+        for i in range(num_n):
+            a.append(v[i])
+            b.append(v[i+num_n])
+            c.append(v[i+2*num_n])
+        df = pd.DataFrame({'Phase a': a, 'Phase b': b, 'Phase c': c})
 
+        # Lưu dataframe ra file Excel
+        df.to_excel("output.xlsx", index=False)
+nrs=NRS('FEEDER901.xlsx')
+res=nrs.report()
+
+
+### chạy 3 pha nếu để các tải giống nhau thì đãng lẽ ra các bus phải có điện áp giống nhau
+
+## lamf database
+## chuan hoa lai du lieu
+## ve luoi
+## viet dan
+##contrain toois uwu rang buoc,  dua vao ham muc tieu. f(x)= -ploss +min(u,105 neu u-105<0 thi cho bang 0 vi dap ung dieu kien )
+##so sanh valid sincal, đồ thị cần thay đổi, viet do an, luoi co nhanh, luoi cua lo 22kV
